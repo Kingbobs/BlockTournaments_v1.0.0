ce96eaa7c12bd5c61c721b8c8c823483d4983406
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-06-06 02:03:13 -0600

    Create EventListener.php

---
+++ Kingbobs/EventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kingbobs;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+
+class EventListener implements Listener{
+
+	public function __construct(MainClass $plugin){
+		$this->plugin = $plugin;
+	}
+
+	/**
+	 * @priority        HIGHEST
+	 * @ignoreCancelled true
+	 */
+	public function blockBreak(BlockBreakEvent $e){
+		// $nb = $this->plugin->getServer()->getPluginManager()->getPlugin("AddNoteBlock");
+		$p = $e->getPlayer();
+		$b = $e->getBlock();
+		if($this->plugin->blockTourney() == true){
+			if(!isset($this->plugin->bT[$p->getName()])){
+				$this->plugin->bT[$p->getName()] = 1;
+			}else{
+				$this->plugin->bT[$p->getName()] = $this->plugin->bT[$p->getName()] + 1;
+			}
+		}
+	}
+}
